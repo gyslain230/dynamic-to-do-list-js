da2f129ec340b7077131ec7ed3b317e9bc9a0886
rgyslain <gislainrugira@gmail.com>
2025-01-30 15:35:28 +0200

    addding final touches

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", ()=>{
     const addButton= document.getElementById("add-task-btn");
     const taskInput= document.getElementById("task-input"); 
     const taskList=document.getElementById("task-list");
     
     //function of controling the adding of tasks
-      loadTasks();
+     loadTasks();
     function loadTasks() {
          const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-        storedTasks.forEach(taskText => addTask); // 'false' indicates not to save again to Local Storage
+        storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
       
     }
     function saveTasks() {
@@ -18,46 +19,43 @@ document.addEventListener("DOMContentLoaded", ()=>{
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
     /////////////////////////////
-    function addTask() {
-        const taskText=taskInput.value.trim();
-        if (taskText.value=="") {
+    function addTask(taskText, save = true) {
+         taskText =taskText || taskInput.value.trim();
+        if (!taskText) {
             alert(' prompt the user to enter a task.'); 
+            return;
             
         }
-        else{
-            const newTask= document.createElement("li");
-            newTask.textContent = taskText;
-            const removeButton= document.createElement("button");
-            removeButton.textContent="Remove";
-            //classname to remove button
-            removeButton.classList.add("remove-btn");
-            //removing the new task wen the remove button is clicked
-            removeButton.onclick=function(){
+        
+        const newTask= document.createElement("li");
+        newTask.textContent = taskText;
+        const removeButton= document.createElement("button");
+        removeButton.textContent="Remove";
+        //classname to remove button
+        removeButton.classList.add("remove-btn");
+        //removing the new task wen the remove button is clicked
+        removeButton.onclick = function(){
                 taskList.removeChild(newTask);
                 saveTasks();
                 
                 
-            }
-            newTask.appendChild(removeButton);
-            taskList.appendChild(newTask);
-            taskInput.value="";
-            saveTasks();
-            
-
-
-
         }
+        newTask.appendChild(removeButton);
+        taskList.appendChild(newTask);
+        taskInput.value="";
+        if (save){
+            saveTasks();
+        }          
+        
        
        
         
     }
      //saving the task list on the local storage system
-    
-
-    // adding functionalities to the add button so as to respond to when enter button is clicked and on click
-    addButton.addEventListener("click",addTask);
-    taskInput.addEventListener("keypress",()=>{
-        if (event.key =="Enter"){
+     // adding functionalities to the add button so as to respond to when enter button is clicked and on click
+    addButton.addEventListener("click",function (){addTask(null,true)});
+    taskInput.addEventListener("keypress",(event)=>{
+        if (event.key ==="Enter"){
             addTask();
             
         }
